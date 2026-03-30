7e53a1487383fa3a020af3ea7c1df44e02e62872
SamuelK0nrad <samuel.ko702@gmail.com>
2026-03-30 22:46:28 +0200

    add simple example handler

--- agent-backend/cmd/api/main.go
+++ agent-backend/cmd/api/main.go
@@ -1,27 +1,7 @@
 package main
 
+import "agent-backend/server"
+
 func main() {
-	// env := config.NewEnv(".env", true)
-
-	// runtime := server.NewRouter(env)
-	// defer func() {
-	// 	if err := runtime.Close(); err != nil {
-	// 		log.Printf("error closing runtime: %v", err)
-	// 	}
-	// }()
-
-	// sigCh := make(chan os.Signal, 1)
-	// signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	// go func() {
-	// 	<-sigCh
-	// 	if err := runtime.Close(); err != nil {
-	// 		log.Printf("error during shutdown: %v", err)
-	// 	}
-	// 	os.Exit(0)
-	// }()
-
-	// err := runtime.Router.Run(":8080")
-	// if err != nil {
-	// 	log.Fatalf("error occurred running the api: %v", err)
-	// }
+	server.NewRouter()
 }
